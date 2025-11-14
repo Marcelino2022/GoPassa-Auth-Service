58d648cc1a1828135eacd4898fcbeaeaa6d3aaad
Manuel Marcelino <manuelmarcelino07@gmail.com>
2025-11-14 00:19:38 +0000

    Improving tenantId validation when registering the user.

--- src/main/java/com/gopassa/auth_service/application/dto/requestDTO/creationDTO/CreateUserDTO.java
+++ src/main/java/com/gopassa/auth_service/application/dto/requestDTO/creationDTO/CreateUserDTO.java
@@ -1,6 +1,5 @@
 package com.gopassa.auth_service.application.dto.requestDTO.creationDTO;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
